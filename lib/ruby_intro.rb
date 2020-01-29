2ed7bd0211455b9543745b5c4ce585fb9fb0f7c7
Junyu Han <jyhan@tamu.edu>
2020-01-28 21:54:23 -0600

    finished part1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,21 +3,44 @@
 # Part 1
 
 def sum arr
-  if (!arr)
+  # reject nil element
+  new_arr = arr.reject { |n| n.nil?}
+  # if arr is empty return zero
+  if (new_arr.empty?)
     return 0
+  # else return the sumation
   else
     sum = 0
-    arr.each{ |n| sum = sum + n}
+    new_arr.each{ |n| sum = sum + n}
     return sum
   end
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  new_arr = arr.reject { |n| n.nil?}
+  if (new_arr.empty?)
+    return 0
+  elsif (new_arr.size == 1)
+    return new_arr[0]
+  else
+    sorted_arr = new_arr.sort
+    return sorted_arr[-1] + sorted_arr[-2]   
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  new_arr = arr.reject { |n| n.nil?}
+  if (new_arr.empty?)
+    return false
+  else
+    h = {}
+    new_arr.each_with_index do |num, index|
+      aim = n - num
+      return true if h[aim]
+      h[num] = index
+    end
+    return false
+  end
 end
 
 # Part 2
